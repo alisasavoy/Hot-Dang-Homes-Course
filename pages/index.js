bca1dc980b0f58173c6a195fd46601fd414d481d
alisasavoy <45887547+alisasavoy@users.noreply.github.com>
2026-09-18 10:39:19 -0700

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+import { Page } from "components/Page";
+import { getPageStaticProps } from "utils/getPageStaticProps";
+
+
+
+export default Page
+
+export const getStaticProps =  getPageStaticProps
